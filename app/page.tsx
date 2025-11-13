d5cc1a294e921a20f4b867460ce75d2026dda8d3
shivam-dubey-byte <dubeyshivamraj@gmail.com>
2025-11-14 01:50:10 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar"
+import Navbar from "@/components/navbar"
 import PlaneAnimation from "@/components/plane-animation"
 import FeaturedDeals from "@/components/featured-deals"
 import Footer from "@/components/Footer"
